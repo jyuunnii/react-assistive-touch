9eebcfe74f43b9b50ad662e8f91fe545c254ccc4
Jynn Park <yuunnii315@gmail.com>
2021-04-03 22:05:33 +0900

    feat: add menu

--- src/AssistiveTouch/AssitiveTouchMenu.tsx
+++ src/AssistiveTouch/AssitiveTouchMenu.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import './styles.css';
+
 const AssitiveTouchMenu = () => {
-    return <div>menu!</div>
+    return (
+    <div className="menu">
+        <ul>
+            <li className="menu-item-1"><i>⭐️</i>menu1</li>
+            <li className="menu-item-2"><i>📱</i>menu2</li>
+            <li className="menu-item-3"><i>💰</i>menu3</li>
+            <li className="menu-item-4"><i>✉️</i>menu4</li>
+            <li className="menu-item-5"><i>🏠</i>menu5</li>
+            <li className="menu-item-6"><i>🔋</i>menu6</li>
+        </ul>
+    </div>)
+    ;
 }
 
 export default AssitiveTouchMenu;
